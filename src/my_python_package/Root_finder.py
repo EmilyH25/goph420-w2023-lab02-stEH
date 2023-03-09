60dadb82892b1053152fd77e9467174159a3c7de
EmilyH25 <emily.hill@ucalgary.ca>
2023-03-09 00:20:28 -0700

    renamed modes to guesses for more accuracy of definition. Troubleshooted all equations to improve functionality. Hardcoded the derivative of the function as the variable function was not working.

--- src/my_python_package/Root_finder.py
+++ src/my_python_package/Root_finder.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 from math import pi
 
+
 def f(zeta,freq):
     
     rho1 = 1800
@@ -15,7 +16,7 @@ def f(zeta,freq):
     under_root = H**2*(Beta1**(-2)-Beta2**(-2))-zeta**2
     tanf = np.tan(2*pi*freq*zeta)
     
-    g = ((rho2/rho1)*(under_root)**(1/2)/zeta)-tanf
+    g = tanf-((rho2/rho1)*(under_root)**(1/2)/zeta)
     return g
     
 def dfdx(zeta, freq):    
@@ -25,33 +26,36 @@ def dfdx(zeta, freq):
     Beta2 = 3200
     H = 4000
     
-    dens_ratio = rho1/rho2
-    under_root = H**2*(Beta1**(-2)-Beta2**(-2))-zeta**2
-    tanf = np.tan(2*pi*freq*zeta)
-    
-    deriv = dens_ratio*((-1/(under_root**0.5))-(under_root**0.5)/zeta**2)-((2*freq*pi)/(np.cos(2*pi*freq*zeta)))
+    root = (16575-(5776*(zeta**2)))**.5
+    addy = 2*freq*pi/((np.cos(2*pi*freq*zeta))**2)
+    deriv = (138125/(456*(zeta**2)))*(1/root)+addy
     return deriv
  
-def modes(freq):
+def guesses(freq):
     ini_guess = []
     n = 1
     guess = 0
     asympt = 0
     while guess < 1.69:
         nasympt = n/(4*freq)
-        guess = (nasympt+asympt)/2
-        asympt = nasympt
+        guess = nasympt
         if guess < 1.69:
             ini_guess.append(guess)
-        n += 1
+        n += 2
     return ini_guess
     
- 
-def newton_raphson(x0,f,dfdx,freq,eps_s=1e-8):
+def newton_raphson(x0,freq,f,dfdx,eps_s=1e-8):
     eps_a = 2*eps_s
     xk = x0
+    error = []
+    niter = 0
+    d = []
     while eps_a > eps_s:
-        dx = -f(xk, freq)/dfdx(xk, freq)
-        xk += dx
-        eps_a = np.abs(dx/xk)
-    return xk
+        xn = xk-f(xk, freq)/dfdx(xk, freq)
+        d.append(xn)
+        eps_a = np.abs((xn-xk)/xn)
+        error.append(eps_a)
+        niter += 1
+        xk=xn
+    return xk, niter, error
+
