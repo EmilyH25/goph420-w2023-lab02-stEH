136bc66b389dd06eeaf6d252e22021df50a83bfd
EmilyH25 <emily.hill@ucalgary.ca>
2023-03-09 23:46:32 -0700

    removed guesses function and replaced it with a list of frequencies and a list of assiciated guesses. Added script that calculates zeta, love wave velocity, and wavelengths, adds them to lists, and plots them against frequency

--- src/my_python_package/Root_finder.py
+++ src/my_python_package/Root_finder.py
@@ -3,7 +3,7 @@
 import numpy as np
 import math
 from math import pi
-
+import matplotlib.pyplot as plt
 
 def f(zeta,freq):
     
@@ -30,19 +30,6 @@ def dfdx(zeta, freq):
     addy = 2*freq*pi/((np.cos(2*pi*freq*zeta))**2)
     deriv = (138125/(456*(zeta**2)))*(1/root)+addy
     return deriv
- 
-def guesses(freq):
-    ini_guess = []
-    n = 1
-    guess = 0
-    asympt = 0
-    while guess < 1.69:
-        nasympt = n/(4*freq)
-        guess = nasympt
-        if guess < 1.69:
-            ini_guess.append(guess)
-        n += 2
-    return ini_guess
     
 def newton_raphson(x0,freq,f,dfdx,eps_s=1e-8):
     eps_a = 2*eps_s
@@ -59,3 +46,44 @@ def newton_raphson(x0,freq,f,dfdx,eps_s=1e-8):
         xk=xn
     return xk, niter, error
 
+frequencies = [0.1,0.5,1,1.5,2,2.5]
+f1_g = [1.4]
+f2_g = [0.4,1.25]
+f3_g = [0.23,0.7,1.15]
+f4_g = [0.159, 0.47, 0.79]
+f5_g = [0.12,0.36,0.6]
+f6_g = [0.099,0.299,0.48]
+
+list = [f1_g,f2_g,f3_g,f4_g,f5_g,f6_g]
+
+zeta_list = []
+for i in range (len(frequencies)):
+    small_list = []
+    for t in range (len(list[i])):
+        zet, iter, error = newton_raphson(list[i][t],frequencies[i],f,dfdx)
+        small_list.append(zet)
+    zeta_list.append(small_list)
+
+vel_list = []
+for i in range (len(frequencies)):
+    small_list = []
+    for t in range (len(zeta_list[i])):
+        cl = (1/((1/1900)**2-((zeta_list[i][t])/4000)**2))**(0.5)
+        plt.plot(frequencies[i],cl,'g^')
+        small_list.append(cl)
+    vel_list.append(small_list)
+plt.xlabel('frequency (Hz)')
+plt.ylabel('velocity (m/s2)')
+plt.show()    
+
+wave_list = []
+for i in range (len(frequencies)):
+    small_list = []
+    for t in range (len(vel_list[i])):
+        wave = (vel_list[i][t])/frequencies[i]
+        plt.plot(frequencies[i], wave, 'bo')
+        small_list.append(wave)
+    wave_list.append(small_list)
+plt.xlabel('frequency (Hz)')
+plt.ylabel('wavelength (m)')
+plt.show()
